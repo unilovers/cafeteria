30102627f1885915073959a5ff7df5034a238254
YuriLotzB <yuribrun@edu.unifil.br>
2025-11-22 21:07:09 -0300

    :sparkles: Criação da entidade Atendente
    Foi criado a entidade Atendente

---
+++ src/main/java/cafeteria/model/entities/Atendente.java
@@ -0,0 +1,67 @@
+package cafeteria.model.entities;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "atendente")
+public class Atendente implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    private String email;
+
+    // Construtor vazio
+    public Atendente() {
+    }
+
+    // Construtor com argumentos
+    public Atendente(Integer id, String nome, String cpf, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    // Getters e Setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
